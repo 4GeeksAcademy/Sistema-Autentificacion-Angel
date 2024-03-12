b41f1551097dc3fa4c4b1eb3f3ed3797698d3982
angeljimenez91 <139405887+angeljimenez91@users.noreply.github.com>
2024-03-12 09:33:28 +0000

    final

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect, useContext, useLayoutEffect } from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { Context, useActions } from "../store/appContext";
 
 export const Navbar = () => {
-const { store, actions } = useContext(Context);
-const [isLogin, setIsLogin] = useState(false);
+  const { store, actions } = useContext(Context);
+  const [isLogin, setIsLogin] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-const handleLogout = (e) => {
+  const handleLogout = (e) => {
     e.preventDefault();
     actions.logout();
     setIsLogin(false);
+    navigate('/login');
   };
 
   useLayoutEffect(() => {
@@ -17,22 +20,25 @@ const handleLogout = (e) => {
     setIsLogin(store.token && store.token !== "" && store.token !== undefined);
   }, [store.token]);
 
+  // Mostrar el Navbar solo en la página privada ("/private")
+  if (location.pathname !== '/private') {
+    return null;
+  }
+
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container">
         <div className="ml-auto">
           {!store.token ? (
-
             <Link to="/">
               <button className="btn btn-primary">Log out</button>
             </Link>
-
-
-          ) : (<Link to="/">
-            <button onClick={handleLogout} className="btn btn-primary">
-              Log out
-            </button>
-          </Link>
+          ) : (
+            <Link to="/">
+              <button onClick={handleLogout} className="btn btn-primary">
+                Log out
+              </button>
+            </Link>
           )}
         </div>
       </div>
--- src/front/js/pages/Private.js
+++ src/front/js/pages/Private.js
@@ -1,23 +1,25 @@
-import React, { useState, useEffect, useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link, useNavigate } from 'react-router-dom';
+import "../../styles/private.css";
 
 const Private = () => {
   const { store, actions } = useContext(Context);
-  const [ infoPrivada, setInfoPrivada] = useState();
+  const [infoPrivada, setInfoPrivada] = useState();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (store.token) {
-      setInfoPrivada('esto es super secreto')
+      setInfoPrivada('Bienvenido')
     }
-  }, [store.token]); 
+  }, [store.token]);
 
 
   return (
-    <div className="hola">
-      <p>{infoPrivada}</p>
+    <div className="info-container">
+      <p className="info-privada">Bienvenido a su página privada</p>
     </div>
+
   )
 
 
